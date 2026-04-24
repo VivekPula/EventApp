6c9e48a92cbbfb9d66b552ab541afbfb073f3d50
Vivek Pula <vivekpula9@gmail.com>
2026-04-24 19:30:02 +0530

    Ticket

--- Frontend/src/pages/BookingPage.jsx
+++ Frontend/src/pages/BookingPage.jsx
@@ -8,8 +8,8 @@ const getData = async (id,setData,setBooked,setLoading)=>{
     try{
         const res = await fetch(`/api/data/${id}`);
         const data = await res.json();
-        const name = localStorage.name;
-        const ticketRes = await fetch(`/api/data/ticket?event=${data.title}&user=${name}`);
+        const user_id = localStorage.id;
+        const ticketRes = await fetch(`/api/data/ticket?event=${data.title}&user_id=${user_id}`);
         const ticketData = await ticketRes.json();
         if(ticketData.NA!=null&&ticketData.NA==="true"){
             setBooked(false);
@@ -24,17 +24,11 @@ const getData = async (id,setData,setBooked,setLoading)=>{
 }
 const book = async (data,id,setBooked) =>{
     const name = localStorage.name;
+    const user_id = localStorage.id;
     const query = {
         name : name,
+        user_id : user_id, 
         title : data.title,
-        date : data.date,
-        language : data.language,
-        price : data.price,
-        time : data.time,
-        location : data.city +", "+data.state,
-        category : data.category,
-        duration : data.duration,
-        description : data.description,
         Eid : id,
     };
     await fetch('/api/data/ticket',{
@@ -100,7 +94,7 @@ const BookingPage = () =>{
     else
         return(
             <div className="w-full h-full flex justify-center"> 
-                <div className="flex flex-col  items-center w-2/3 h-7/10 m-2 p-2 my-auto  rounded-2xl border-4 border-(--primaryColor)/10 darkMode:bg-(--accentColor)/60" >
+                <div className="flex flex-col  items-center w-2/3 h-8/11 m-2 p-2 my-auto  rounded-2xl border-4 border-(--primaryColor)/10 darkMode:bg-(--accentColor)/60" >
                     <p className="w-9/10 text-5xl font-medium border-b pb-2 border-b-gray-200 text-center text-(--primaryColor)/80 darkMode:text-(--secondaryColor)">{data.title}</p>
                     <div className="flex gap-10 mt-10 text-xl justify-center w-8/10">
                         <div className="flex flex-col gap-5 w-1/2 ml-15 ">
@@ -113,12 +107,16 @@ const BookingPage = () =>{
                         <p className="flex gap-2 items-center min-w-1/3 "> <Languages className="text-(--accentColor) darkMode:text-(--exColor)/80"/> : {data.language}</p>
                         <p className="flex gap-2 items-center min-w-1/3 "> <Grid3x2 className="text-(--accentColor) darkMode:text-(--exColor)/80"/> : {data.category}</p>
                         <p className="flex gap-2 items-center min-w-1/3 "> <IndianRupee className="text-(--accentColor) darkMode:text-(--exColor)/80"/> : {data.price}</p>
-                        <p className="flex gap-2 items-center min-w-1/3 "> <MapPin className="text-(--accentColor) darkMode:text-(--exColor)/80"/> : {data.location}</p>
+                        <p className="flex gap-2 items-center min-w-1/3 "> <MapPin className="text-(--accentColor) darkMode:text-(--exColor)/80"/> : {data.city+", "+data.state}</p>
                         </div>
                     </div>
-                    <div className=" flex gap-4 items-center px-5 mb-5 mt-auto rounded-xl text-green-500 border">
+                    
+                    <div className=" flex gap-4 items-center px-5 mb-2 mt-auto rounded-xl text-green-500 border">
                         <TicketCheck size={60}/>
                         <p className="text-5xl font-bold">Ticket Booked</p>
+                    </div>
+                    <div>
+                        <p>Booked At : {data.createdAt.slice(0,10)+", "+data.createdAt.slice(11,19)}</p>
                     </div>
                     
                 </div>
