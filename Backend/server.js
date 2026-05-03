cb2fcf1e5ca9ee442811cc5f9f12550cc52be0c6
Vivek Pula <vivekpula9@gmail.com>
2026-05-03 10:59:32 +0530

    .env stuff

---
+++ Backend/server.js
@@ -0,0 +1,35 @@
+// Importing dependencies
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+dotenv.config();
+
+// Importing utils
+import logInfo from "./utils/logInfo.js";
+
+// Importing Routes
+import userRoute from "./Routes/user.js";
+import eventRoute from "./Routes/event.js";
+import createEventRoute from "./Routes/createEvent.js";
+
+const app = express();
+app.use(express.json());
+const port = 5000;
+mongoose
+  .connect(
+    process.env.DATABASE_URL,
+  )
+  .then(() => console.log("Db connected"))
+  .catch((err) => console.log(err));
+
+//middleware that logs info
+app.use(logInfo);
+//Routes to the matching route-prefix
+app.use("/auth", userRoute); // Requests starting with /auth are redirected to userRoute
+app.use("/api/data", eventRoute); // Requests starting with /api/data are redirected to eventRoute
+app.use("/api/createevent",createEventRoute);
+app.use("/api/img",express.static(`${import.meta.dirname}/uploads`));
+app.listen(port, () => {
+  console.log(`listening in ${port}`);
+});
